f68fefab935abc369eb1218d13d2ecd3fd836395
lowhum <95083372+lowhum@users.noreply.github.com>
2025-06-18 13:10:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,26 @@ import datetime
 
 st.title("📚 ЦМБ Каталог статии ➔ RIS конвертиране")
 
+st.markdown("""
+1. Каталог на ЦМБ: http://nt-cmb.mu-sofia.bg """)
+st.markdown("""
+2. Каталог на ЦМБ: http://nt-cmb.mu-sofia.bg """)
+
 
 user_text = st.text_area("Копирайте записите тук :", height=400, value='')
 
 if 'ris_exp' not in st.session_state:
     st.session_state['ris_exp'] = ''
 
-if st.button("🔄 Конвертирай в RIS"):
+if st.button("🔄 Конвертиране в RIS"):
     converter = RobustRISConverter()
     ris = converter.process_text_to_ris(user_text)
     st.session_state['ris_exp'] = ris.strip()
     if ris.strip() and "No valid bibliographic" not in ris:
-        st.success(f"Конветирани са {ris.count('TY  - JOUR')} запис(а) ")
+        st.success(f"Конвертирани са {ris.count('TY  - JOUR')} запис(а) ")
         st.text_area("RIS Output", value=st.session_state['ris_exp'], height=250)
     else:
-        st.warning("Не са открити валисни записи.")
+        st.warning("Не са открити валидни записи.")
 
 if st.session_state['ris_exp']:
     st.download_button(
